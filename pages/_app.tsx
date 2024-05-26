bbc127a77b6420177c2158af725081798ce216cd
chainemesis <topdash2030@gmail.com>
2024-05-26 11:57:42 -0700

    Ton

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,36 @@ import Footer from './Footer';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <TonConnectUIProvider
-        manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
-      >
+      manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
+      uiPreferences={{ theme: THEME.DARK }}
+      walletsListConfiguration={{
+        includeWallets: [
+          {
+            appName: "safepalwallet",
+            name: "SafePal",
+            imageUrl:
+              "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
+            aboutUrl: "https://www.safepal.com/download",
+            jsBridgeKey: "safepalwallet",
+            platforms: ["ios", "android", "chrome", "firefox"],
+          },
+          {
+            appName: "tonwallet",
+            name: "TON Wallet",
+            imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+            aboutUrl:
+              "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+            universalLink: "https://wallet.ton.org/ton-connect",
+            jsBridgeKey: "tonwallet",
+            bridgeUrl: "https://bridge.tonapi.io/bridge",
+            platforms: ["chrome", "android"],
+          },
+        ],
+      }}
+      actionsConfiguration={{
+        twaReturnUrl: "https://t.me/tc_twa_demo_bot/start",
+      }}
+    >
       <SnackbarProvider>
         <Header />
         <Component {...pageProps} />
